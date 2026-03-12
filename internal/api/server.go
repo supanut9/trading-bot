a907a470ab2945b1b91379b21cc586ab96face42
Supanut Tansorajprasert <91820751+supanut9@users.noreply.github.com>
2026-03-12 14:22:03 +0700

    feat: add cors middleware to support remote dashboard connection

--- internal/api/server.go
+++ internal/api/server.go
@@ -30,10 +30,29 @@ type APIServer struct {
 	PanicCh   chan bool
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func NewAPIServer() *APIServer {
 	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.Use(CORSMiddleware())
+
 	s := &APIServer{
-		router:    gin.Default(),
+		router:    router,
 		clients:   make(map[*websocket.Conn]bool),
 		positions: []Position{},
 		PanicCh:   make(chan bool, 1),
